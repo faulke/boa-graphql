8a5cfff422b52c6db5f6373f744f06905e46c9a0
Evan Faulk <faulkev@gmail.com>
2019-01-07 19:53:37 -0800

    Add user query

--- src/apollo/createSchema.ts
+++ src/apollo/createSchema.ts
@@ -2,10 +2,14 @@ import { buildSchema } from 'type-graphql'
 import { GraphQLSchema } from 'graphql'
 
 import { RecipeResolver } from  '../resolvers/recipe'
+import { UserResolver } from '../resolvers/user'
 
 async function createSchema(): Promise<GraphQLSchema> {
   const schema = await buildSchema({
-    resolvers: [RecipeResolver]
+    resolvers: [
+      RecipeResolver,
+      UserResolver
+    ]
   })
 
   return schema
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,8 @@
 import {
   Resolver,
-  Query
+  Query,
+  Arg,
+  ID
 } from 'type-graphql'
 import { Repository } from 'typeorm'
 import { InjectRepository } from 'typeorm-typedi-extensions'
@@ -8,7 +10,7 @@ import { InjectRepository } from 'typeorm-typedi-extensions'
 import { User } from '../entities/user'
 
 @Resolver(of => User)
-export class RecipeResolver {
+export class UserResolver {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>
   ) {}
@@ -16,5 +18,10 @@ export class RecipeResolver {
   @Query(returns => [User], { description: 'Get all users.' })
   async users(): Promise<User[]> {
     return this.userRepository.find()
+  }
+
+  @Query(returns => User, { nullable: true, description: 'Get single user by id.'})
+  async user(@Arg('userId', type => ID) userId: string): Promise<User> {
+    return this.userRepository.findOne(userId)
   }
 }
